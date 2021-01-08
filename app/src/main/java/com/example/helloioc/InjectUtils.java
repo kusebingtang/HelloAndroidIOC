84a3b7655459175e4ee9c47ca75364a8374fc85e
kusebintang <kusebintang@163.com>
2021-01-08 17:42:43 +0800

    finish 控件的注入

--- app/src/main/java/com/example/helloioc/InjectUtils.java
+++ app/src/main/java/com/example/helloioc/InjectUtils.java
@@ -1,5 +1,8 @@
 package com.example.helloioc;
 
+import android.view.View;
+
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 public class InjectUtils {
@@ -7,6 +10,33 @@ public class InjectUtils {
         //布局的注入
         injectLayout(context);
 
+        //控件的注入
+        injectView(context);
+
+    }
+
+    private static void injectView(Object context) {
+        Class<?> aClass = context.getClass();
+        //反射获取Activity的属性对象列表
+        Field[] fields = aClass.getDeclaredFields();
+
+        for (Field field : fields) {
+            ViewInject viewInject = field.getAnnotation(ViewInject.class);
+            if (viewInject != null) {//Field 对象有 @ViewInject 注解
+                int viewId = viewInject.value();
+                //运行到这里，每个按钮的ID已经取到了-->viewId
+                //注入就是反射执行findViewById方法
+                try {
+                    Method method=aClass.getMethod("findViewById",int.class);
+                    //通过反射方法调用，找到view
+                    View findView = (View) method.invoke(context, viewId);
+                    field.setAccessible(true);//private 压制
+                    field.set(context,findView);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     private static void injectLayout(Object context) {
@@ -18,9 +48,9 @@ public class InjectUtils {
             layout = contentView.value();
             try {
                 //setContentView 反射获取
-                Method method = context.getClass().getMethod("setContentView",int.class);
-                method.invoke(context,layout);//context.setContentView(layout)
-            }catch (Exception e) {
+                Method method = context.getClass().getMethod("setContentView", int.class);
+                method.invoke(context, layout);//context.setContentView(layout)
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
--- app/src/main/java/com/example/helloioc/MainActivity.java
+++ app/src/main/java/com/example/helloioc/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.helloioc;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +9,10 @@ import android.widget.Toast;
 @ContentView(R.layout.activity_main)
 public class MainActivity extends BaseActivity implements View.OnClickListener {
 
+    @ViewInject(R.id.button1)
     private Button btn1;
+
+    @ViewInject(R.id.button2)
     private Button btn2;
 
 
@@ -19,8 +20,8 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
-        btn1 = findViewById(R.id.button1);
-        btn2 = findViewById(R.id.button2);
+//        btn1 = findViewById(R.id.button1);
+//        btn2 = findViewById(R.id.button2);
 
         btn1.setText("我是按钮1");
         btn2.setText("我是按钮2");
